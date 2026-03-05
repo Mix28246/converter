ebd79edabd45d0eae4912ce5fae7d0beba946d1a
MiX <96899379+Mix28246@users.noreply.github.com>
2026-03-05 19:17:32 +0700

    feat: добавлены новые конвертации

--- converters.c
+++ converters.c
@@ -19,3 +19,12 @@ float hours_to_minutes(float h) {
 float gb_to_mb(float gb) {
     return gb * 1024.0;
 }
+
+float psi_to_bar(float psi) {
+    return psi * 0.0689476;
+}
+
+float degrees_to_radians(float deg) {
+    return deg * (3.14159 / 180.0);
+}
+
