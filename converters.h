d080f79901e8eacef24e440eb9c8e7962ae3e7e2
MiX <96899379+Mix28246@users.noreply.github.com>
2026-03-05 18:10:54 +0700

    feat: создание прототипов функций

---
+++ converters.h
@@ -0,0 +1,12 @@
+#ifndef CONVERTERS_H
+#define CONVERTERS_H
+
+
+float celsius_to_fahrenheit(float c);
+float kg_to_pounds(float kg);
+
+
+float meters_to_kilometers(float m);
+float liters_to_gallons(float l);
+
+#endif
